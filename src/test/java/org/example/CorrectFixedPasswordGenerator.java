d08c057083ed90df06e4f91951ad0251b52ef3f9
Baek Gi Chan <laphayen@gmail.com>
2024-02-28 16:14:08 +0900

    Implement low-coupling design

---
+++ src/test/java/org/example/CorrectFixedPasswordGenerator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class CorrectFixedPasswordGenerator implements PasswordGenerator{
+
+    @Override
+    public String generatePassword() {
+        return "abcdefgh";
+    }
+}
