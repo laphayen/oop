728e3c21dc792fbc2ffa3a382e06227f381a7bcd
Baek Gi Chan <laphayen@gmail.com>
2024-03-03 00:09:59 +0900

    Chore: division operation

--- src/main/java/org/example/calculate/DivisionOperator.java
+++ src/main/java/org/example/calculate/DivisionOperator.java
@@ -8,6 +8,9 @@ public class DivisionOperator implements NewArithmeticOperator {
 
     @Override
     public int calculate(int operand1, int operand2) {
+        if (operand2 == 0) {
+            throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+        }
         return operand1 / operand2;
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CalculatorTest {
@@ -30,4 +31,13 @@ public class CalculatorTest {
                 arguments(4, "/", 2, 2)
         );
     }
+
+    @DisplayName("")
+    @Test
+    void calculateExceptionTest() {
+        assertThatCode(() -> Calculator.calculate(10, "/", 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("0으로 나눌 수 없습니다.");
+    }
+
 }
