49d206de50b82d656961a862b42ce3c9410f0082
Baek Gi Chan <laphayen@gmail.com>
2024-03-02 23:17:34 +0900

    Refactor: Addition operation

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Calculator {
     public static int calculate(int operand1, String operator, int operand2) {
-        return operand1 + operand2;
+        if ("+".equals(operator)) {
+            return operand1 + operand2;
+        }
+        return 0;
     }
 }
