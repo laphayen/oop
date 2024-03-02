3f4651889e6c68952ca3f837907366eaf4cbb42f
Baek Gi Chan <laphayen@gmail.com>
2024-03-02 23:46:38 +0900

    Refactor: enum method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class Calculator {
     public static int calculate(int operand1, String operator, int operand2) {
-        if ("+".equals(operator)) {
-            return operand1 + operand2;
-        }
-        return 0;
+        return ArithmeticOperator.calculate(operand1, operator,operand2);
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -2,16 +2,32 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CalculatorTest {
 
     @DisplayName("덧셈 연산 수행")
-    @Test
-    void additionTest() {
-        int result = Calculator.calculate(1, "+", 2);
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void calculateTest(int operand1, String operator, int operand2, int result) {
+        int calculateResult = Calculator.calculate(operand1, operator,operand2);
 
-        assertThat(result).isEqualTo(3);
+        assertThat(calculateResult).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
     }
 }
