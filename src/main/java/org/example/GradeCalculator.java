98d643934ac0b647a5874a5e1927459e48c8543d
Baek Gi Chan <laphayen@gmail.com>
2024-03-03 21:18:29 +0900

    Test: Calculate average credits

---
+++ src/main/java/org/example/GradeCalculator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.List;
+
+public class GradeCalculator {
+    private final List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public double calculateGrade() {
+        return 4.5;
+    }
+}
