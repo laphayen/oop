4c86c928b72a4d90b259abb764dcf07037519391
Baek Gi Chan <laphayen@gmail.com>
2024-03-05 22:18:27 +0900

    feat: Implement the order() function for the customer

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Customer {
     public void order(String menuName, Menu menu, Cooking cooking) {
-
+        MenuItem menuItem = menu.choose(menuName);
+        Cook cook = cooking.makeCook(menuItem);
     }
 }
